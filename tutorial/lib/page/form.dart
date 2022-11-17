fc0ef1689b463f3de41f543ee73f3085d0b85d75
Monica Oktaviona <monicaoktaviona10@gmail.com>
2022-11-17 13:13:19 +0700

    Menyelesaikan Lab 8

--- tutorial/lib/page/form.dart
+++ tutorial/lib/page/form.dart
@@ -1,6 +1,6 @@
 import 'package:tutorial/main.dart';
 import 'package:flutter/material.dart';
-
+import 'package:tutorial/page/to_do_page.dart';
 
 class MyFormPage extends StatefulWidget {
   const MyFormPage({super.key});
@@ -29,6 +29,44 @@ class _MyFormPageState extends State<MyFormPage> {
       appBar: AppBar(
         title: Text('Form'),
       ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            // Menambahkan clickable menu
+            ListTile(
+              title: const Text('Counter'),
+              onTap: () {
+                // Route menu ke halaman utama
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Form'),
+              onTap: () {
+                // Route menu ke halaman form
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+
       body: Form(
         key: _formKey,
         child: SingleChildScrollView(
